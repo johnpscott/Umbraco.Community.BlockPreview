8cc63e338c9afc7c148661828efc2383842cbcc8
John Paul Scott <johnpscott@live.co.uk>
2025-01-25 15:59:45 +0000

    Strip content and insert minimal content to reproduce the issue

---
+++ src/Umbraco.Cms.15.x/Views/Partials/blockgrid/Components/headline.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BGPreview.Views.Partials.blockgrid.components
+{
+    public class headlineModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
